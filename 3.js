3b9ed09572407c0114eb648abf7a85a2ff59a170
Hasan Omar <r3domar1@gmail.com>
2025-10-27 20:22:40 +0200

    added task 3

---
+++ 3.js
@@ -0,0 +1,7 @@
+// חסן עומר + עיסא לואבנה
+
+
+const arr = [5, 10, 15,0, 20, 25,0,3,4,0,5,0,1,0]; // 5 zeros
+
+const zerosCounts = arr.filter(e => Boolean(e) === false).length;
+console.log(zerosCounts)
