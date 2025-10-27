cfeb07d7cfc4c7bb7cb7161c4495ded4c1517e64
Hasan Omar <r3domar1@gmail.com>
2025-10-27 16:43:52 +0200

    added first+second tasks

---
+++ first.js
@@ -0,0 +1,10 @@
+// חסן עומר + עיסא לואבנה
+
+const num = 30;
+
+let r1 = num % 2 === 0;
+let r2 = num % 3 === 0;
+let r3 = num % 5 === 0;
+
+let result = r1 + r2 + r3;
+console.log(result);
---
+++ second.js
@@ -0,0 +1,11 @@
+// חסן עומר + עיסא לואבנה
+
+for (let i = 2; i < 237; i++) {
+  let isPrime = true;
+
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j === 0) isPrime = false;
+  }
+
+  if (isPrime) console.log(i);
+}
