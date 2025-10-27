0c3c226607ebaa927babc5217949d97fb31b221b
Hasan Omar <r3domar1@gmail.com>
2025-10-27 20:34:57 +0200

    updated

--- 3.js
+++ 3.js
@@ -1,7 +1,7 @@
 // חסן עומר + עיסא לואבנה
 
+const arr = [5, 10, 15, 0, 20, 25, 0, 3, 4, 0, 5, 0, 1, 0]; // 5 zeros
 
-const arr = [5, 10, 15,0, 20, 25,0,3,4,0,5,0,1,0]; // 5 zeros
-
-const zerosCounts = arr.filter(e => Boolean(e) === false).length;
-console.log(zerosCounts)
+const zerosCounts = arr.filter((e) => e === 0).length;
+// const zerosCounts = arr.filter((e) => Boolean(e) === false).length;
+console.log(zerosCounts);
