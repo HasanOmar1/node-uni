b76772fcd5d448d03f056beb91cd107554dec5f1
Hasan Omar <r3domar1@gmail.com>
2025-10-29 12:42:00 +0200

    added

---
+++ week2/app.js
@@ -0,0 +1,5 @@
+// app.js
+const sayHello = require("./hello");
+
+sayHello.sayHello(); // print "Hello, world!"
+sayHello.sayHello2(); // print "Hello, world 222222!"
---
+++ week2/hello.js
@@ -0,0 +1,10 @@
+// myModule.js
+const sayHello = () => {
+  console.log("Hello, world!");
+};
+
+const sayHello2 = () => {
+  console.log("Hello, world 2222222222!");
+};
+
+module.exports = { sayHello, sayHello2 };
