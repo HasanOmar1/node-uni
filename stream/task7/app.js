ba1f1fc8f90ce5be25e2be275bbbf7f9ffef55bf
Hasan Omar <r3domar1@gmail.com>
2025-11-14 12:40:57 +0200

    added HW 6

--- stream/task7/app.js
+++ stream/task7/app.js
@@ -1,43 +1,58 @@
 const fs = require("fs");
 const path = require("path");
 
-const dirPath = path.join(__dirname, "/text");
+const dirPath = path.join(__dirname, "text");
 
-const arrOfFiles = ["1.txt", , "2.txt"];
+const arrOfFiles = ["1.txt", "2.txt", "3.txt"];
 
 const fn = () => {
-  for (let i = 1; i < arrOfFiles.length; i++) {
-    let random = Math.ceil(Math.random() * 20);
+  let maxLines = 0;
+
+  // --- CREATE INPUT FILES (random content) ---
+  for (let i = 0; i < arrOfFiles.length; i++) {
+    let randomLines = Math.ceil(Math.random() * 20);
     let arrOfRandomNums = [];
-    for (let j = 0; j < random; j++) {
+
+    maxLines = Math.max(maxLines, randomLines);
+
+    for (let j = 0; j < randomLines; j++) {
       let randomNumsInFile = Math.ceil(Math.random() * 100);
       arrOfRandomNums.push(randomNumsInFile);
     }
+
     let data = arrOfRandomNums.join("\n");
-    // console.table(data);
-    fs.writeFileSync(`${dirPath}/${i}.txt`, data);
-    arrOfRandomNums = [];
+    fs.writeFileSync(`${dirPath}/${i + 1}.txt`, data, "utf8");
   }
 
-  let max = 0;
+  // --- READ ALL FILES INTO ARRAYS OF LINES ---
+  let fileLines = arrOfFiles.map((file, idx) => {
+    let data = fs.readFileSync(`${dirPath}/${idx + 1}.txt`, "utf-8");
+    return data.length === 0 ? [] : data.split("\n");
+  });
 
-  for (let i = 1; i < arrOfFiles.length; i++) {
-    let arrOfLines = [];
-    let dataByPerLine = fs.readFileSync(`${dirPath}/${i}.txt`, "utf-8");
-    console.log(dataByPerLine);
-    console.log("****************");
+  let result = [];
 
-    // arrOfLines.push(dataByPerLine);
-    // console.table(arrOfLines);
-    // console.log(dataByPerLine.length);
-    // if (arrOfLines.length > max) {
-    //   max = arrOfLines.length;
-    // }
+  let takeCount = 1; // how many lines to take this round
 
-    // fs.appendFileSync(`${dirPath}/output.txt`, split[c] + "\n");
+  while (true) {
+    let nothingToTake = true;
+
+    for (let f = 0; f < fileLines.length; f++) {
+      for (let k = 0; k < takeCount; k++) {
+        if (fileLines[f].length > 0) {
+          nothingToTake = false;
+          result.push(fileLines[f].shift()); // remove first line
+        }
+      }
+    }
+
+    if (nothingToTake) break; // all files empty
+    takeCount++; // next round take more lines
   }
 
-  // console.log(max);
+  // --- WRITE OUTPUT FILE ---
+  const outputPath = path.join(dirPath, "output.txt");
+  fs.writeFileSync(outputPath, result.join("\n"));
 };
 
 fn();
