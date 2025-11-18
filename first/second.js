9d1600e0462a2f99b5a06e19d948a17694ac2787
Hasan Omar <r3domar1@gmail.com>
2025-11-18 18:07:59 +0200

    added comments

--- first/second.js
+++ first/second.js
@@ -1,22 +1,17 @@
 // חסן עומר + עיסא לואבנה
 
+// Function to check if a number is prime
 const isPrime = (num) => {
+  // Loop from 2 up to √num (optimization)
   for (let i = 2; i <= Math.sqrt(num); i++) {
+    // If divisible, it's not prime
     if (num % i === 0) return false;
   }
+  // If loop finishes → number is prime
   return true;
 };
 
+// Loop from 2 to 236 and print all prime numbers
 for (let i = 2; i < 237; i++) {
   if (isPrime(i)) console.log(i);
 }
-
-// for (let i = 2; i < 237; i++) {
-//   let isPrime = true;
-
-//   for (let j = 2; j <= Math.sqrt(i); j++) {
-//     if (i % j === 0) isPrime = false;
-//   }
-
-//   if (isPrime) console.log(i);
-// }
