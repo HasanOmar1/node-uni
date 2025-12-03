0658513c811f7e0130f0942d52ba04651a92de7c
Hasan Omar <r3domar1@gmail.com>
2025-12-03 12:22:45 +0200

    added task 8

--- express/task8/app.js
+++ express/task8/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const fs = require("fs");
+const data = require("./data");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get("/products/:productPrice", (req, res) => {
+  const { productPrice } = req.params;
+  const products = data.products.filter((d) => d.price > productPrice);
+  res.send(products);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
