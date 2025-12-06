4d8ff86bf91906298e02bb396f0c63b66d4f0a0f
Hasan Omar <r3domar1@gmail.com>
2025-12-06 11:32:38 +0200

    added check for minPrice > maxPrice

--- express/task9/app.js
+++ express/task9/app.js
@@ -1,30 +1,37 @@
 const express = require("express");
-const data = require("./data");
+const { products } = require("./data");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/api/products", (req, res) => {
-  const { name, minPrice, maxPrice, limit } = req.query;
+  let { name, minPrice, maxPrice, limit } = req.query;
 
-  let products = [];
+  let filteredProducts = [...products];
   if (name) {
-    products = data.products.filter((d) =>
-      d.name.toLowerCase().includes(name.toLowerCase())
+    filteredProducts = filteredProducts.filter((d) =>
+      d.name.toLowerCase().startsWith(name.toLowerCase())
     );
   }
 
-  if (minPrice) {
-    products = data.products.filter((d) => d.price > minPrice);
+  if (minPrice && maxPrice && +minPrice > +maxPrice) {
+    temp = minPrice;
+    minPrice = maxPrice;
+    maxPrice = temp;
   }
-  console.log("**********");
+
+  if (minPrice) {
+    filteredProducts = filteredProducts.filter((d) => d.price > +minPrice);
+  }
 
   if (maxPrice) {
-    products = data.products.filter((d) => d.price < maxPrice);
+    filteredProducts = filteredProducts.filter((d) => d.price < +maxPrice);
   }
-  console.log(products);
+  if (limit) {
+    filteredProducts = filteredProducts.slice(0, +limit);
+  }
 
-  res.send(products);
+  res.send(filteredProducts);
 });
 
 app.listen(port, () => {
